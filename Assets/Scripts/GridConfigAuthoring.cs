2b033707f638d3df363e9cc463fb8710f5bdb707
Keijiro Takahashi <keijiro@gmail.com>
2023-09-25 16:30:40 +0900

    Meshes

--- Assets/Scripts/GridConfigAuthoring.cs
+++ Assets/Scripts/GridConfigAuthoring.cs
@@ -4,15 +4,19 @@ using Unity.Entities;
 public class GridConfig : IComponentData
 {
     public uint GridCount;
+    public float PointRadius;
     public Mesh GridMesh;
-    public Material GridMaterial;
+    public Mesh PointMesh;
+    public Material Material;
 }
 
 public class GridConfigAuthoring : MonoBehaviour
 {
     public uint GridCount = 4;
+    public float PointRadius = 0.1f;
     public Mesh GridMesh;
-    public Material GridMaterial;
+    public Mesh PointMesh;
+    public Material Material;
 
     class GridConfigBaker : Baker<GridConfigAuthoring>
     {
@@ -21,8 +25,10 @@ public class GridConfigAuthoring : MonoBehaviour
             var data = new GridConfig()
             {
                 GridCount = src.GridCount,
+                PointRadius = src.PointRadius,
                 GridMesh = src.GridMesh,
-                GridMaterial = src.GridMaterial
+                PointMesh = src.PointMesh,
+                Material = src.Material
             };
             AddComponentObject(GetEntity(TransformUsageFlags.None), data);
         }
