9b6e658f1299bd2fef855124006d949478ffb0bb
Keijiro Takahashi <keijiro@gmail.com>
2023-10-06 16:43:10 +0900

    Split sample point jobs

--- Assets/Scripts/Systems/SamplePoint.cs
+++ Assets/Scripts/Systems/SamplePoint.cs
@@ -17,19 +17,42 @@ public partial struct SamplePointUpdateSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        var job = new SamplePointUpdateJob
+        var job1 = new SamplePointUpdateJob
+        {
+            Triangle = SystemAPI.GetSingleton<Triangle>()
+        };
+
+        var job2 = new SamplePointRenderElementJob
         {
             Space = SystemAPI.GetSingleton<GridSpace>(),
             Appear = SystemAPI.GetSingleton<Appearance>(),
             Colors = SystemAPI.GetSingleton<ColorScheme>(),
             Triangle = SystemAPI.GetSingleton<Triangle>()
         };
-        job.ScheduleParallel();
+
+        state.Dependency = job1.Schedule(state.Dependency);
+        state.Dependency = job2.Schedule(state.Dependency);
     }
 }
 
 [BurstCompile]
 partial struct SamplePointUpdateJob : IJobEntity
+{
+    public Triangle Triangle;
+
+    void Execute(in Layer layer,
+                 in PixelCoords coords,
+                 in SamplePoint point,
+                 ref SampleResult result)
+    {
+        var pos = point.GetPosition(layer, coords);
+        var hit = TriangleUtil.TestPoint(pos, Triangle);
+        result = new SampleResult{ Hit = hit };
+    }
+}
+
+[BurstCompile]
+partial struct SamplePointRenderElementJob : IJobEntity
 {
     public GridSpace Space;
     public Appearance Appear;
@@ -39,23 +62,19 @@ partial struct SamplePointUpdateJob : IJobEntity
     void Execute(in Layer layer,
                  in PixelCoords coords,
                  in SamplePoint point,
-                 ref SampleResult result,
+                 in SampleResult result,
                  ref RenderElement render)
     {
         var pos = point.GetPosition(layer, coords);
-        var hit = TriangleUtil.TestPoint(pos, Triangle);
-
         var center = point.GetPosition(new Layer{Index = 0}, coords);
         var snap = MathUtil.smootherstep(Appear.SamplePointSnap);
         pos = math.lerp(center, pos, snap);
 
-        var color = hit ? Colors.HitColor : Colors.MissColor;
+        var color = result.Hit ? Colors.HitColor : Colors.MissColor;
         color = LayerUtil.ApplyAlpha(color, layer, Appear.ActiveLayer);
 
         var scale = Appear.SamplePointRadius;
         scale *= AnimUtil.Scan(Space, coords, Appear.SamplePointParam);
-
-        result = new SampleResult{ Hit = hit };
 
         render = new RenderElement
         {
